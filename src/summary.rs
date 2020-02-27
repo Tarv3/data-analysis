1622099eb679d0ab9683e89dc0c1f3c5e3e44223
Josh <a1748908@student.adelaide.edu.au>
2020-02-27 18:02:01 +1030

    Added variance to summary

--- src/summary.rs
+++ src/summary.rs
@@ -7,6 +7,7 @@ pub fn write_summary_headers(prefix: &str, mut writer: impl Write) -> std::io::R
 
 pub struct Summary<T> {
     pub avg: T,
+    pub variance: T,
     pub min: Option<T>,
     pub max: Option<T>,
     pub quartiles: Option<[T; 3]>
@@ -16,8 +17,6 @@ impl<T> Summary<T>
 where
     T: Average + Clone,
 {
-    
-
     pub fn from_iter<O>(values: impl Iterator<Item = T>, mut ordering: O) -> Summary<T>
     where
         O: FnMut(&T, &T) -> Ordering,
@@ -25,6 +24,7 @@ where
         let mut values: Vec<T> = values.collect();
 
         let avg = T::average(values.iter().cloned());
+        let variance = T::variance(&avg, values.iter().cloned());
         let min = values.iter().cloned().min_by(&mut ordering);
         let max = values.iter().cloned().max_by(&mut ordering);  
         
@@ -38,6 +38,7 @@ where
 
         Summary {
             avg,
+            variance,
             min,
             max,
             quartiles
